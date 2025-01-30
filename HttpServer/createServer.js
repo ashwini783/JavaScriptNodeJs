aa0d3d3f6fffd54b2b56b34a1cbed2976edb166e
Ashwini <ashwiniavhad8@gmail.com>
2025-01-31 01:11:49 +0530

    new changes

--- HttpServer/createServer.js
+++ HttpServer/createServer.js
@@ -4,11 +4,22 @@ const server=http.createServer((req,res)=>{
     //handling incoming request and sending response back to client
 
     //server is telling what kind of output client can accept
-    res.setHeader('Content-Type','text/plain')
+    // res.setHeader('Content-Type','text/plain')
+    // res.write("Hello World")
+    // res.write("Bye")
+
     // res.setHeader('Content-Type','text/html')
     // res.write("<html><h1>Hi</h1></html")
-    res.write("Hello World")
-    res.write("Bye")
+    
+    res.setHeader("Content-Type","application/json")
+    const jsonData={
+        message:"Hello World",
+        date:new Date()
+    }
+    const jsonResponse=JSON.stringify(jsonData)
+    res.write(jsonResponse)
+
+   
     res.end()
 })
 
