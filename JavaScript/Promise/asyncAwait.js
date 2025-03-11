c2312bb79d9bed66444c51ac5a655f226c9a9ff3
Ashwini <ashwiniavhad8@gmail.com>
2025-03-11 23:56:06 +0530

    code try catch

--- JavaScript/Promise/asyncAwait.js
+++ JavaScript/Promise/asyncAwait.js
@@ -29,13 +29,18 @@ let p2=new Promise((resolve)=>{
 })
 //below function will be suspended from CallStack for 10 sec i.e took out of callStack and after some time it will come back again in callStack~
 async function resolvePromise2(){ //for 10sec resolvePromise2() function is suspended from callStack so that other function will get executed
-    console.log("Hello World")
-    let data1=await p2;
-    console.log("from p2")
-    console.log(data1)
-     let data2=await p1;  // await resolve the promise
-     console.log("Hello inside promise2")
-     console.log(data2)
+     try{
+        console.log("Hello World")
+        let data1=await p2;
+        console.log("from p2")
+        console.log(data1)
+         let data2=await p1;  // await resolve the promise
+         console.log("Hello inside promise2")
+         console.log(data2)
+     }
+     catch(error){
+        console.log(error)
+     }
 }
 resolvePromise2()
 
