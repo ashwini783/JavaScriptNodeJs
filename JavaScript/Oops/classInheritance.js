48c6fd1effc7b6f72158911b9e3468c27d3c1fb2
Ashwini <ashwiniavhad8@gmail.com>
2025-03-13 10:12:09 +0530

    static method

--- JavaScript/Oops/classInheritance.js
+++ JavaScript/Oops/classInheritance.js
@@ -10,6 +10,17 @@ class Car{
     drive(){
         console.log(`I am driving ${this.brand} ${this.name}`) //this method is part of prototype,not the part of object
     }
+   static compareSpeed(car1,car2){
+        if(car1.topSpeed>car2.topSpeed){
+            console.log(`${car1.name} is faster than ${car2.name}`)
+        }
+        else if(car1.topSpeed<car2.topSpeed){
+            console.log(`${car2.name} is faster than ${car1.name}`)
+        }
+        else{
+            console.log(`${car1.name} and ${car2.name} have same speed`)
+        }
+    }
  }
  class ElectricCar extends Car{
        constructor(Brand,Name,Color,TopSpeed,BatteryCapacity,TimeToCharge){
@@ -20,5 +31,8 @@ class Car{
  }
 
  const car1=new Car('Mercedes','G-Wagon','Red','300km/hr')
+ 
+ const car2=new Car('Tata','Punch','Black','200km/hr')
+ Car.compareSpeed(car1,car2)
  const evCar=new ElectricCar('Tesla','Models-S','White','200km/hr','100kwh','3hrs')
  console.log(evCar)
