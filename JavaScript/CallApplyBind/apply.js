f72722421cc088e78ae9c5cac7835620d464f9be
Ashwini <ashwiniavhad8@gmail.com>
2025-03-15 21:36:12 +0530

    apply with Math.max added

--- JavaScript/CallApplyBind/apply.js
+++ JavaScript/CallApplyBind/apply.js
@@ -15,3 +15,11 @@
   
   //when we want to send or store data in bulk we can use apply() method so we can use array to pass the values
  person1.printDetails.apply(person2,['swimming','football'])
+
+ //how math.max() work with apply()
+ let max=Math.max(1,2,3,4)
+ console.log(max) //Math.max accept the separate values 
+
+ let arr=[2,5,3,8,10] //to find max of array , apply() method will unpack the elements of array
+ let maxElem=Math.max.apply(null,arr);
+ console.log(maxElem)
