412ed9f61cad5f10a0f37d5d225caa216961a264
Ashwini <ashwiniavhad8@gmail.com>
2025-03-16 21:34:17 +0530

    promise.all polyfills added

---
+++ JavaScript/Promise/promise.allPolyfills.js
@@ -0,0 +1,37 @@
+
+
+Promise.myAll=function(values){
+    const promise=new Promise(function(resolve,reject){
+        // if(Array.isArray(values)){
+            let result=[]
+            let total=0;
+            values.forEach(function(item,index){
+                Promise.resolve(item).then((res)=>{
+                    result[index]=res
+                    total++;
+                    if(total===values.length){  //resolved promises==to incoming promise array length
+                         resolve(result)
+                    }
+                }).catch((err)=>{
+                    reject(err)
+                })
+            })
+
+        // }
+        // else{
+        //     console.error('An array has not been passed!')
+        // }
+    })
+    return promise
+
+}
+let promise1=[
+    Promise.resolve(1),
+    Promise.resolve(2),
+    Promise.resolve(3)
+]
+Promise.myAll([promise1]).then((results)=>{
+    console.log("Result is ",results[0])
+}).catch((error)=>{
+    console.log('Error ',error)
+})
