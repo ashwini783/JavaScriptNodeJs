279b57a92110f190081572a967ee7f88a5124ebe
Ashwini <ashwiniavhad8@gmail.com>
2025-03-16 21:45:16 +0530

    promise.all polyfills added

--- JavaScript/Promise/promise.allPolyfills.js
+++ JavaScript/Promise/promise.allPolyfills.js
@@ -1,9 +1,9 @@
 
 
 Promise.myAll=function(values){
-    const promise=new Promise(function(resolve,reject){
+    const promise=new Promise(function(resolve,reject){  //promise.all returns a single promise so we have created promise here
         // if(Array.isArray(values)){
-            let result=[]
+            let result=[] //used to store the resolved value
             let total=0;
             values.forEach(function(item,index){
                 Promise.resolve(item).then((res)=>{
