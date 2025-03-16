dde1b7e85d008ae85872db98ddf0057d07658457
Ashwini <ashwiniavhad8@gmail.com>
2025-03-16 16:17:00 +0530

    deep copy and shallow copy

--- JavaScript/JsDataTypeMemory/shallowCopy.js
+++ JavaScript/JsDataTypeMemory/shallowCopy.js
@@ -20,7 +20,13 @@ zoo1.animals[1].species="Elephant" //shallow copy only handles the first layer o
 let zooString=JSON.stringify(zoo1) //now this is separate string
 console.log(zooString)
 let deepCopy=JSON.parse(zooString) //JSON.parse will again create separate object,new reference for object created
-deepCopy.animals[0].species="Tiger";
+deepCopy.animals[0].species="Tiger"; //nothing changed in original object
 deepCopy.location="Kerala India"
+let zoo2=zoo1
 console.log(zoo1)
 console.log(deepCopy)
+console.log(zoo1===zoo2) //true
+console.log(zoo1===zoo1shallowCopy) //false
+console.log(zoo1.animals===zoo1shallowCopy.animals) //true
+console.log(zoo1===deepCopy) //false
+console.log(zoo1.animals===deepCopy.animals) //false
