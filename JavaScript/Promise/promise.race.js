ed63d0b3affde006cce40a5187c2c78681d6ebc4
Ashwini <ashwiniavhad8@gmail.com>
2025-03-16 12:22:56 +0530

    promise.race added

---
+++ JavaScript/Promise/promise.race.js
@@ -0,0 +1,18 @@
+let p1=function quickResolve(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{ resolve("Qickly Resolved") },500)
+    })
+}
+
+let p2=function slowResolveOrFastReject(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>resolve("slowly resolved"),2000)
+        // setTimeout(()=>reject(new Error("Quick Rejected!")))
+    })
+}
+
+//doesn't matter it is resolved or reject it will give u first result i.e which will execute first
+//whatever promise will serve first 
+Promise.race([p1(),p2()]).then((data)=>{
+    console.log("result ",data)
+}).catch((error)=>console.log("Error ",error))
