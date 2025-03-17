f7b4fc945952eb35d6d5ce3df0216bebd1953f1d
Ashwini <ashwiniavhad8@gmail.com>
2025-03-17 09:21:01 +0530

    filter polyfills

--- JavaScript/Arrays/filterPolyfills.js
+++ JavaScript/Arrays/filterPolyfills.js
@@ -1,8 +1,8 @@
-let arr=[21,8,24,33,45,100,2]
-let evenNum=arr.filter((val)=>{
-    return val%2==0
-})
-console.log(evenNum)
+let arr = [21, 8, 24, 33, 45, 100, 2];
+let evenNum = arr.filter((val) => {
+  return val % 2 == 0;
+});
+console.log(evenNum);
 //filter
 //1.returns the new array
 //2.put in conition
@@ -11,15 +11,17 @@ console.log(evenNum)
 //5.callback-condition
 //6.iterate on your array
 
-Array.prototype.myFilter=function(callback){
-    let resultArr=[]
-        for(let i=0;i<this.length;i++){
-          if(callback(this[i])){ //callback(this[i]) either return true or false         resultArr.push(this[i])
-          }
-        }
-        return resultArr
-}
-let oddElem=arr.myFilter(function(val){
-     return val%2!=0
-})
-console.log(oddElem)
+Array.prototype.myFilter = function (callback) {
+  let resultArr = [];
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i])) {//callback(this[i]) either return true or false         
+    resultArr.push(this[i])
+         
+     }
+  }
+  return resultArr;
+};
+let oddElem = arr.myFilter(function (val) {
+  return val % 2 != 0;
+});
+console.log(oddElem);
