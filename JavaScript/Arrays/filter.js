aec792ce8b0dd93a0c095bdbd0ea48d3eeb1191d
Ashwini <ashwiniavhad8@gmail.com>
2025-03-17 08:58:04 +0530

    filter method added

--- JavaScript/Arrays/filter.js
+++ JavaScript/Arrays/filter.js
@@ -1,8 +1,8 @@
 
 
 let nums=[1,34,36,57,33,22,56,78,9]
-
-let evenNum=nums.filter((num)=>num%2==0)
+// Returns the elements of an array that meet the condition specified in a callback function.
+let evenNum=nums.filter((num)=>num%2==0) //return type is array
 console.log(evenNum)
 
 let deositArr=[1000,3000,1500,-1999,-2000,3300]
